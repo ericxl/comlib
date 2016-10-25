78ca8b8c8a654f8a445accdee3169ef42a25d15b
Eric Liang <jklelxy@gmail.com>
2016-10-25 16:44:07 -0500

    Added populate data into ilist extensions

--- extensions/IEnumerableExtensions.cs
+++ extensions/IEnumerableExtensions.cs
@@ -5,9 +5,8 @@ using System.Linq;
 public static class IEnumerableExtensions
 {
     /// <summary>
-    ///   Shuffles a list in O(n) time by using the Fisher-Yates/Knuth algorithm.
+    /// Shuffles a list in O(n) time by using the Fisher-Yates/Knuth algorithm.
     /// </summary>
-    /// <param name="r"></param>
     /// <param name = "list"></param>
     public static void Shuffle<T>(this IList<T> list)
     {
@@ -29,6 +28,46 @@ public static class IEnumerableExtensions
         if (list == null && other == null) return true;
         if (list.Count != other.Count) return false;
         return !list.Where((t, i) => Newtonsoft.Json.JsonConvert.SerializeObject(t) != Newtonsoft.Json.JsonConvert.SerializeObject(other[i])).Any();
+    }
+
+    public static void PopulateDataBy<TData, TController>(this IList<TController> goList, IList<TData> list, Func<TData, TController> AddFactory, Action<TController, TData> UpdateFactory,
+        Action<TController> CleanUpFactory)
+    {
+        var iteration = Math.Min(goList.Count, list.Count);
+        var maxIteration = Math.Max(goList.Count, list.Count);
+        var deleteGo = goList.Count > list.Count;
+        if (UpdateFactory != null)
+        {
+            for (var i = 0; i < iteration; i++)
+            {
+                UpdateFactory(goList[i], list[i]);
+            }
+        }
+
+        if (deleteGo)
+        {
+            for (var i = iteration; i < maxIteration; i++)
+            {
+                var go = goList.Last();
+                if (CleanUpFactory != null)
+                {
+                    CleanUpFactory(go);
+                }
+                goList.RemoveAt(goList.Count - 1);
+            }
+        }
+        else
+        {
+            for (var i = iteration; i < maxIteration; i++)
+            {
+                var added = AddFactory(list[i]);
+                if (UpdateFactory != null)
+                {
+                    UpdateFactory(added, list[i]);
+                }
+                goList.Add(added);
+            }
+        }
     }
 }
 
