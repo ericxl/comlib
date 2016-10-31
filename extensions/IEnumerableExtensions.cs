2cd54582267f9d9b28f4e7fca912e51977ffd10a
Eric Liang <jklelxy@gmail.com>
2016-10-30 21:48:45 -0500

    Added DateTime extensions

--- extensions/IEnumerableExtensions.cs
+++ extensions/IEnumerableExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 public static class IEnumerableExtensions
 {
@@ -19,6 +20,12 @@ public static class IEnumerableExtensions
             list[j] = list[i];
             list[i] = temp;
         }
+    }
+
+    public static T RandomElement<T>(this IList<T> array)
+    {
+        var index = new Random().Next(0, array.Count);
+        return array[index];
     }
 
     public static bool Equals<T>(this IList<T> list, IList<T> other)
@@ -69,6 +76,37 @@ public static class IEnumerableExtensions
                 goList.Add(added);
             }
         }
+    }
+
+    /// <summary>
+    /// Calls ToString() on every element of the list, puts [encapsulate] directly before and after the result
+    /// and then concatenates the results with [seperator] between them.
+    /// </summary>
+    /// <typeparam name="T">The collection element type.</typeparam>
+    /// <param name="list">The collection to concatenate.</param>
+    /// <param name="separator">The seperator between entries.</param>
+    /// <param name="encapsulate">The string to put directly before and after every item.</param>
+    /// <returns>A string containing the ToString() results of all items.</returns>
+    public static string ToOneLineString<T>(this IEnumerable<T> list, string separator = ", ", string encapsulate = "\"")
+    {
+        var useEncapsulate = encapsulate.Length > 0;
+
+        var result = new StringBuilder();
+        foreach (var element in list)
+        {
+            if (result.Length > 0)
+                result.Append(separator);
+
+            if (useEncapsulate)
+                result.Append(encapsulate);
+
+            result.Append(element);
+
+            if (useEncapsulate)
+                result.Append(encapsulate);
+        }
+
+        return result.ToString();
     }
 }
 
