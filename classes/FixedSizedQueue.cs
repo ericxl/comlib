8307bf2f8a266a50394e35841c7d8f3f509299d6
Eric Liang <ericliang@utexas.edu>
2017-03-28 23:20:52 -0500

    added FixedSizedQueue

---
+++ classes/FixedSizedQueue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+public class FixedSizedQueue<T> : Queue<T>
+{
+    public FixedSizedQueue(int size)
+    {
+        Size = size;
+    }
+    public int Size { get; private set; }
+    public new void Enqueue(T obj)
+    {
+        base.Enqueue(obj);
+        lock (this)
+        {
+            while (base.Count > Size)
+            {
+                base.Dequeue();
+            } ;
+        }
+    }
+}
